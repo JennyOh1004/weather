2be6f4795c4435e189b8851d270dc0ef8b34a53d
“JennyOh1004” <ohseulki1004@gmail.com>
2020-01-07 00:56:43 +0000

    Add bootstrap

--- app/controllers/ozones_controller.rb
+++ app/controllers/ozones_controller.rb
@@ -1,4 +1,12 @@
 class OzonesController < ApplicationController
 	def index
+		require 'net/http'
+		require 'json'
+
+		@url = 'http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=95136&distance=0&API_KEY=9A3ECA20-6988-41DB-B707-FF9419F5B014'
+		@uri = URI(@url)
+		@response = Net::HTTP.get(@uri)
+		@output = JSON.parse(@response)
+
 	end
 end
